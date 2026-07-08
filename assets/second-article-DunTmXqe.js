37ba3e1832298dca317775abb67b478e90f8fe5c
Ilnur Gabitov <i.gabitov2077@gmail.com>
2026-07-08 15:18:19 +0800

    Updates

---
+++ assets/second-article-DunTmXqe.js
@@ -0,0 +1,12 @@
+var e=`# Second Article
+
+Here is the *second* article. It showcases a different set of markdown features.
+
+## Subheading
+
+Some more text with **bold**, *italic*, and \`code\`.
+
+\`\`\`js
+console.log('Hello, world!');
+\`\`\`
+`;export{e as default};
